7afe536e5c580da4dc8409f6dea0ff619b4b4671
Vlad <123529192+PaRoVoZ0@users.noreply.github.com>
2025-11-28 10:54:11 +0300

    Обнова

--- Laba4/laba4.cpp
+++ Laba4/laba4.cpp
@@ -1,36 +1,49 @@
 #include <iostream>
 
-int main(){
+int main() {
     setlocale(LC_ALL, "rus");
 
-    int arr_1[5], arr_2[5], s = 5, cnt = 0, res[5];
+    int arr_1[5], arr_2[5], s = 5, cnt_fact = 0, cnt_user;
 
-    std::cout << "Введите элементы превого массива:" << "\n";
-    for (int i = 0; i < s; i++){
-        std::cin >> arr_1[i];
+    std::cout << "Введите кол-во минимальных элементов, которые не входят во второй массив (не больше 5): "; std::cin >> cnt_user;
+
+    for (int i = 0; i < s; i++) {
+        arr_1[i] = rand() % 10;
+        arr_2[i] = rand() % 10;
     }
 
-    std::cout << "Введите элементы второго массива:" << "\n";
-    for (int i = 0; i < s; i++){
-        std::cin >> arr_2[i];
+    std::cout << "Элементы первого массива: ";
+    for (int i = 0; i < s; i++) {
+        std::cout << arr_1[i] << " ";
     }
+    std::cout << "\n";
+
+    std::cout << "Элементы второго массива: ";
+    for (int i = 0; i < s; i++) {
+        std::cout << arr_2[i] << " ";
+    }
+    std::cout << "\n";
 
     int temp_cnt = 0;
-    for (int i = 0; i < s; i++){
-        for (int j = 0; j < s; j++){
-            if (arr_1[i] != arr_2[j]){
+    for (int i = 0; i < s; i++) {
+        for (int j = 0; j < s; j++) {
+            if (arr_1[i] != arr_2[j]) {
                 temp_cnt++;
             }
         }
 
-        if (temp_cnt == 5){
-            res[i] = arr_1[i];
-            cnt++;
+        if (temp_cnt == 5) {
+            cnt_fact++;
+            std::cout << "Найден мин элемент первого массива, не входящий во второй: " << arr_1[i] << "\n";
         }
 
         temp_cnt = 0;
     }
 
-    std::cout << "Результат - ";
-    for (int i = 0; i < cnt; i++) std::cout << res[i] << " ";
+    if (cnt_fact == cnt_user) {
+        std::cout << "Все круто! Количество элементов совпадает с введенным!!";
+    }
+    else {
+        std::cout << "Все плёха :( ! Количество элементов не совпадает с введенным (" << cnt_fact << ")";
+    }
 }
