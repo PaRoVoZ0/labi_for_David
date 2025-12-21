284a920ecba5a3247f40cbb1269ee69a4757b925
Vlad <123529192+PaRoVoZ0@users.noreply.github.com>
2025-12-21 21:47:25 +0300

    POEZDA

--- Laba10/laba10.c
+++ Laba10/laba10.c
@@ -1,77 +1,75 @@
 #include <windows.h>
 
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            
-            // Устанавливаем цвет текста (красный в данном примере)
-            SetTextColor(hdc, RGB(255, 0, 0));
-            
-            // Устанавливаем прозрачный фон текста
-            SetBkMode(hdc, TRANSPARENT);
-            
-            // Получаем размеры клиентской области окна
-            RECT rect;
-            GetClientRect(hWnd, &rect);
-            
-            // Выводим текст в центре окна
-            const char* text = "Hello, GDI!";
-            DrawText(hdc, text, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
-            
-            EndPaint(hWnd, &ps);
-        }
-        break;
-        
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-        
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-    const char CLASS_NAME[] = "SampleWindowClass";
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nCmdShow) {
+    const char CLASS_NAME[] = "MyWindowClass";
     
-    WNDCLASSA wc = {0};
+    WNDCLASS wc = {0};
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     
-    RegisterClassA(&wc);
+    RegisterClass(&wc);
     
-    HWND hWnd = CreateWindowExA(
-        0,
-        CLASS_NAME,
-        "GDI Text Example",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 400, 300,
-        NULL, NULL, hInstance, NULL
+    HWND hwnd = CreateWindowEx(
+        0, CLASS_NAME, "GDI Text Output",
+        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 600, NULL, NULL, hInstance, NULL
     );
     
-    if (hWnd == NULL)
-        return 0;
-    
-    ShowWindow(hWnd, nCmdShow);
-    UpdateWindow(hWnd);
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
     
     MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
+    while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
     
-    return (int)msg.wParam;
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            
+            // Устанавливаем прозрачный фон для текста
+            SetBkMode(hdc, TRANSPARENT);
+            
+            // Устанавливаем цвет текста (красный)
+            SetTextColor(hdc, RGB(255, 0, 0));
+            
+            // Получаем размеры клиентской области окна
+            RECT rect;
+            GetClientRect(hwnd, &rect);
+            
+            // Текст для вывода
+            const char* text = "Hello, GDI!";
+            
+            // Вычисляем размер текста
+            SIZE textSize;
+            GetTextExtentPoint32(hdc, text, strlen(text), &textSize);
+            
+            // Вычисляем координаты для центрирования текста
+            int x = (rect.right - textSize.cx) / 2;
+            int y = (rect.bottom - textSize.cy) / 2;
+            
+            // Выводим текст
+            TextOut(hdc, x, y, text, strlen(text));
+            
+            EndPaint(hwnd, &ps);
+            return 0;
+        }
+        
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+    
+    return DefWindowProc(hwnd, msg, wParam, lParam);
 }
